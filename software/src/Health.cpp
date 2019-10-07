d7f0427147cd77b2091ddec3913e8871e6061d9a
Rachel Misbin <rachelmisbin@pitt.edu>
2019-10-07 12:24:34 -0400

    Resolve Health.cpp merge and updated readme with sensors

--- software/src/Health.cpp
+++ software/src/Health.cpp
@@ -1,9 +1,5 @@
 #include "Health.hpp"
 
-<<<<<<< HEAD
-
-Health::Health()
-=======
 Health::Health() :
     Sensor()
 {
@@ -11,7 +7,6 @@ Health::Health() :
 }
 
 Health::~Health()
->>>>>>> 4df6e6dd0825eea7cc700fb2756b8a9ea36d1c26
 {
 
 }
@@ -21,7 +16,7 @@ bool Health::init()
     return false;
 }
 
-<<<<<<< HEAD
+
 HealthData Health::read()
 {
     HealthData data;
@@ -34,16 +29,6 @@ HealthData Health::poll()
     HealthData data;
 
     return data;
-=======
-std::vector<float> Health::read()
-{
-    return std::vector<float>();
-}
-
-std::vector<float> Health::poll()
-{
-    return std::vector<float>();
->>>>>>> 4df6e6dd0825eea7cc700fb2756b8a9ea36d1c26
 }
 
 void Health::enable()
@@ -55,7 +40,3 @@ void Health::disable()
 {
 
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 4df6e6dd0825eea7cc700fb2756b8a9ea36d1c26
