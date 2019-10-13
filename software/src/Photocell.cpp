d426da1c94da68277daf33454877ac2b0d7243d1
egilman45 <egilman45@users.noreply.github.com>
2019-10-13 11:07:39 -0400

    Update Photocell.cpp

--- software/src/Photocell.cpp
+++ software/src/Photocell.cpp
@@ -7,21 +7,66 @@ Photocell::Photocell() :
 
 }
 
+//While using testing code with the sensor, a button was used to simulate separation of the rocket and code was written based off this
+// In place of gathering launch data from other sensors
+
+
 bool Photocell::init()
 {
-    return false;
+    //All the pins the photocells are connected to
+    int photocellPin1 = 0; 
+    int photocellPin2 = 1;
+    int photocellPin3 = 4;
+    int photocellPin4 = 23;
+
+    int mode = 0;
+    int baseline;
+
+
+    void setup(void) {
+        Serial.begin(9600);
+        pinMode(0, INPUT);
+    }
+    
 }
 
 std::vector<float> Photocell::read()
 {
     std::vector<float> ret(PHOTOCELL_DIMENSIONS, 0);
+    {
+        void loop(void) {
+        int raw = ((analogRead(0)+analogRead(1)+analogRead(4)+analogRead(23))/4) //Average of all Photocell readings
 
+        if (digitalRead(4) == LOW)
+        {
+            mode = !mode;
+            delay(1000);
+        }
+        
+        }
+    }
     return ret;
 }
 
 std::vector<float> Photocell::poll()
 {
     std::vector<float> ret(PHOTOCELL_DIMENSIONS, 0);
+    {
+        if (mode === 0) //prelaunch
+        {
+            baseline = raw;
+            Serial.print("baseline: ");
+            Serial.println(baseline);
+        }
+        else if (mode == 1) //Indicates launch
+        {
+            int diff = raw - baseline;
+            Serial.print("Launch ");
+            Serial.println(diff)
+        }
+    }
+    delay(500)
+
 
     return ret;
 }
