18ba6b373a95a08c698c511b3c34fe0c1aa40c4d
Rachel Misbin <rachelmisbin@pitt.edu>
2019-11-14 17:46:47 -0500

    Updated main from subscale-telemetry (I promise I will eventually implement git modules up in here)

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,50 +1,133 @@
 #include <Arduino.h>
-
-#include "GPS.hpp"
-#include "IMU.hpp"
+#include "Data.hpp"
 #include "Altimeter.hpp"
+#include "IMU.hpp"
 #include "Logger.hpp"
-#include "Photocell.hpp"
-#include "XBeePro.hpp"
-#include "Rfm95w.hpp"
-#include "Health.hpp"
-#include <Metro.h>
+#include "Metro.h"
+#include "AnalogDevices.hpp"
+//#include "LaunchDetect.hpp"
 
+//#define NO_CATCHUP
+
+IMU gyro(true);
+Altimeter alt;
 Logger logger;
+AnalogDevices ad;
+//LaunchDetect launchDetect;
 
-GPS* gps;
-IMU* imu0;
-Altimeter* altimeter;
-Photocell* photocell;
-Health* health;
+Data state;
 
-std::vector<Sensor*> sensors;
+Metro log_flush;
+
+int mode;
+int lastmode;
+int swtch = 0;
+
+void ready();
+void armed();
+
 void setup()
-{    
+{
+    pinMode(13, OUTPUT);
+    pinMode(33, INPUT_PULLUP);
+
+    digitalWrite(13, HIGH);
+
+    //delay(1000);
+
     Serial.begin(9600);
+    delay(1000);
 
-    while (!Serial)
-    {
-        ; // wait for serial port to connect. Needed for native USB.
-    }
-    
-    sensors.push_back(gps);
-    // sensors.push_back(imu0);
-    sensors.push_back(altimeter);
-    sensors.push_back(photocell);
-    sensors.push_back(health);
+    log_flush.setInterval(5000);
 
-    for (Sensor* s : sensors)
-    {
-        logger.addSensor(s);
-    }
-
+    // Initialize sensors and logger
+    gyro.init();
+    alt.init();
+    alt.setBaselinePressure();
     logger.init();
+    ad.init();
+    //launchDetect.init();
+
+    logger.addSensor(&gyro);
+    logger.addSensor(&alt);
+    logger.addSensor(&ad);
+    //logger.addSensor(&launchDetect);
+
+    digitalWrite(13, LOW);
 }
 
 void loop()
 {
-    // logger.log();
+    lastmode = mode;
+    mode = digitalRead(33);
 
-    delay(1000);
+    if (lastmode != mode)
+    {
+        digitalWrite(13, 1);
+        delay(100);
+        digitalWrite(13, 0);
+        delay(100);
+        swtch = 1;
+    }
+
+    if (mode == 1)
+    {
+        ready();
+    }
+    else if (mode == 0)
+    {
+        //Serial.println("armed");
+        armed();
+    }
+
+    if (log_flush.check())
+    {
+        digitalWrite(13, 1);
+        logger.flush();
+        digitalWrite(13, 0);
+    }
+}
+
+void ready()
+{
+    if (swtch)
+    {
+        //logger.close();
+        swtch = 0;
+    }
+}
+
+void armed()
+{
+    if (swtch)
+    {
+        digitalWrite(13, 1);
+        delay(100);
+        digitalWrite(13, 0);
+        delay(100);
+        digitalWrite(13, 1);
+        delay(100);
+        digitalWrite(13, 0);
+        delay(100);
+        digitalWrite(13, 1);
+        delay(100);
+        digitalWrite(13, 0);
+        delay(100);
+        //logger.reopen();
+        swtch = 0;
+    }
+
+    state = gyro.poll(state);
+    state = alt.poll(state);
+    state = ad.poll(state);
+    //state = launchDetect.poll(state);
+
+    /*Serial.print(state.imuData.euler_abs_orientation_x);
+    Serial.print(", ");
+    Serial.print(state.imuData.euler_abs_orientation_y);
+    Serial.print(", ");
+    Serial.print(state.imuData.euler_abs_orientation_z);
+    Serial.println();*/
+
+    logger.log();
 }
